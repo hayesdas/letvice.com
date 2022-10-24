e61c893ecba26ab91f52da7d82bc5e3b8dc2e4dc
hayesdas <107646307+hayesdas@users.noreply.github.com>
2022-10-24 15:05:16 +0300

    Оптимизация ссылок

--- app/Http/Controllers/Admin/AdminController.php
+++ app/Http/Controllers/Admin/AdminController.php
@@ -9,10 +9,6 @@ use Illuminate\Http\Request;
 class AdminController extends Controller
 {
     public function index(){
-        $all_product_count = Product::count();
-        return view('admin', [
-            'all_product_count' => $all_product_count,
-            'products' => Product::all(),
-        ]);
+        
     }
 }
