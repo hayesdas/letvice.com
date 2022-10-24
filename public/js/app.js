690291fc39eda066aeac1c4f28245bc7e88713f4
maxnnn1900 <maxnnn1900@gmail.com>
2022-10-24 14:17:22 +0300

    fix category

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,12 @@
-$(document).ready((function(){$(".logo").click((function(){location.href="/"}))}));
+/******/ (() => { // webpackBootstrap
+var __webpack_exports__ = {};
+/*!*****************************!*\
+  !*** ./resources/js/app.js ***!
+  \*****************************/
+$(document).ready(function () {
+  $('.logo').click(function () {
+    location.href = '/';
+  });
+});
+/******/ })()
+;
